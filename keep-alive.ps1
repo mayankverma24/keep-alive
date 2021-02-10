2ee2d9dd6ddb41469c741d0ca246e2ee12903e03
Mayank Verma <mayankverma786@gmail.com>
2021-02-10 13:22:45 -0800

    Add files via upload

--- keep-alive.ps1
+++ keep-alive.ps1
@@ -1,8 +1,8 @@
 clear host
 #
 # Script to keep the PC alive, will prevent screen lock and sleep.
-# Works by pressing Print Screen every 60 minutes, side effect is that a screenshot will overwrite the clipboard contents
-#
+# Works by pressing ESC key every 3 minutes
+# How to: Create a new folder named as keep-alive on Desktop or any other directory and keep both the files in that folder
 
 $opt = (Get-Host).PrivateData
 $opt.WarningBackgroundColor = "DarkCyan"
@@ -11,8 +11,8 @@ $opt.WarningForegroundColor = "white"
 write-warning "Your PC will not go to sleep whilst this window is open..."
 Do {
 [void][System.Reflection.Assembly]::LoadWithPartialName('System.Windows.Forms')
-[System.Windows.Forms.SendKeys]::SendWait("{PRTSC}")
+[System.Windows.Forms.SendKeys]::SendWait("{ESC}")
 
-Start-Sleep -Seconds 60
+Start-Sleep -Seconds 180
 
 } While ($true)
